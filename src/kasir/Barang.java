dfcd41ecb284f5023a09f024d892b19bb75ac3a4
donihamdani <72194437+donihamdani@users.noreply.github.com>
2020-12-21 20:29:55 +0700

    Update Barang.java
    .

--- src/kasir/Barang.java
+++ src/kasir/Barang.java
@@ -23,17 +23,21 @@ public class Barang extends koneksi implements KelolaBarang {
 	
 	//Menu Data
     public void Menu() {
-        System.out.println("\n--MENU DATA BARANG--");
-        System.out.println("1. Tambah Barang");
-        System.out.println("2. Cari Barang");
-        System.out.println("3. Ubah Barang");
-        System.out.println("4. Hapus Barang");
-        System.out.println("5. Lihat Data Barang");
-        System.out.println("0. Kembali");
-        System.out.print("Masukkan Pilihan : ");
+        System.out.println("+==================================+");
+    	System.out.println("|         MENU DATA BARANG         |");
+    	System.out.println("+==================================+");
+        System.out.println("| 1. Tambah Barang                 |");
+        System.out.println("| 2. Cari Barang                   |");
+        System.out.println("| 3. Ubah Barang                   |");
+        System.out.println("| 4. Hapus Barang                  |");
+        System.out.println("| 5. Lihat Data Barang             |");
+        System.out.println("| 0. Kembali                       |");
+        System.out.println("+==================================+");
+        System.out.print("| Masukkan Pilihan : ");
 
         try {
             Integer pilih = input1.nextInt();
+	    System.out.println("+==================================+");
             switch (pilih) {
                 case 0:
                     user.user_pilih();
@@ -54,7 +58,10 @@ public class Barang extends koneksi implements KelolaBarang {
                 	LihatData();
                 	break;
                 default:
-                    System.out.println("Pilihan yang anda masukkan tidak valid!");
+                    System.out.println("+-----------------------------------------+");
+                    System.out.println("| Pilihan yang anda masukkan tidak valid! |");
+                    System.out.println("+-----------------------------------------+\n");
+                    Menu();
             }
         } 
         catch (Exception e) {
@@ -70,9 +77,10 @@ public class Barang extends koneksi implements KelolaBarang {
 		
 		DataBarang brg = new DataBarang();
 		
-		System.out.println(" ------------------------------------------- ");
-		System.out.println("|                TAMBAH DATA                |");
-		System.out.println(" ------------------------------------------- \n");
+		System.out.print("\n\n");
+		System.out.println("+==================================+");
+    		System.out.println("|           TAMBAH DATA            |");
+    		System.out.println("+==================================+");
 		
 		sku = brg.sku();
 		nama = brg.nama();
@@ -85,7 +93,9 @@ public class Barang extends koneksi implements KelolaBarang {
             String sql = "INSERT INTO data_master VALUES ('"+sku+"', '"+nama+"', '"+stock+"', '"+harga_beli+"', '"+harga_jual+"')";   
 
             stmt.execute(sql);
-            System.out.println("\nData Berhasil Tersimpan");
+            System.out.println("+-------------------------+");
+            System.out.println("| Data Berhasil Tersimpan |");
+            System.out.println("+-------------------------+\n");
     
         } 
         catch (Exception e) {
@@ -103,9 +113,11 @@ public class Barang extends koneksi implements KelolaBarang {
 		
 		ArrayList <DataBarang> data = new ArrayList<>();
 		
-        System.out.println("\n ------------------------------------------------------------------------------------ ");
-        System.out.println("|                                    PENCARIAN DATA                                  |");
-        System.out.println(" ------------------------------------------------------------------------------------ ");
+        	System.out.print("\n\n");
+		System.out.println("+==========================================+");
+    		System.out.println("|             PENCARIAN DATA               |");
+    		System.out.println("+==========================================+");
+		  System.out.print("| Masukkan nama barang yang ingin di cari : ");
         
 		System.out.print("Masukkan nama barang yang ingin di cari : ");
 		String cari = input.nextLine();
@@ -191,56 +203,67 @@ public class Barang extends koneksi implements KelolaBarang {
 		String sql;
 		DataBarang brg = new DataBarang();
 		
-        System.out.println("\n -------------------------------------------- ");
-        System.out.println("|                 UBAH BARANG                |");
-        System.out.println(" -------------------------------------------- ");
-        
-        System.out.println("1. Nama");
-        System.out.println("2. Stock");
-        System.out.println("3. Harga Beli");
-        System.out.println("4. Harga Jual");
-		System.out.print("\nPilih data yang ingin diubah : ");
+        	System.out.print("\n\n");
+		System.out.println("+==========================================+");
+    		System.out.println("|               UBAH BARANG                |");
+    		System.out.println("+==========================================+");
+        	System.out.println("| 1. Nama                                  |");
+        	System.out.println("| 2. Stock                                 |");
+        	System.out.println("| 3. Harga Beli                            |");
+        	System.out.println("| 4. Harga Jual                            |");
+        	System.out.println("+==========================================+");
+		System.out.print("| Pilih data yang ingin diubah : ");
 		Integer ubah = input1.nextInt();
-		System.out.println(" ");
+		System.out.println("+==========================================+");
 		
         stmt = conn.createStatement();
         
         try {
 			switch (ubah) {
-				case 1 : 
+				case 1 :
+					System.out.println("+---------------------------------------------+");
 					System.out.print("Masukkan SKU pada data yang ingin di ubah : ");
 					this.sku = input.nextLine();
+					System.out.println("+---------------------------------------------+");
 			        nama = brg.nama();
 			        
 			        sql = "UPDATE data_master SET nama='"+nama+"' WHERE sku='"+sku+"'";   
 			        stmt.execute(sql);
 					break;
 				case 2 :
+					System.out.println("+---------------------------------------------+");
 					System.out.print("Masukkan SKU pada data yang ingin di ubah : ");
 					this.sku = input.nextLine();
+					System.out.println("+---------------------------------------------+");
 			        stock = brg.stock();
 			        
 			        sql = "UPDATE data_master SET stock='"+stock+"' WHERE sku='"+sku+"'";   
 			        stmt.execute(sql);
 					break;
 				case 3 :
+					System.out.println("+---------------------------------------------+");
 					System.out.print("Masukkan SKU pada data yang ingin di ubah : ");
 					this.sku = input.nextLine();
+					System.out.println("+---------------------------------------------+");
 			        harga_beli = brg.harga_beli();
 			        
 			        sql = "UPDATE data_master SET harga_beli='"+harga_beli+"' WHERE sku='"+sku+"'";   
 			        stmt.execute(sql);
 					break;
 				case 4 :
+					System.out.println("+---------------------------------------------+");
 					System.out.print("Masukkan SKU pada data yang ingin di ubah : ");
 					this.sku = input.nextLine();
+					System.out.println("+---------------------------------------------+");
 			        harga_jual = brg.harga_jual();
 			        
 			        sql = "UPDATE data_master SET harga_jual='"+harga_jual+"' WHERE sku='"+sku+"'";   
 			        stmt.execute(sql);
 					break;
 				default :
-					System.out.println("Pilihan Tidak Tersedia\n");
+					System.out.println("+------------------------+");
+					System.out.println("| Pilihan Tidak Tersedia |");
+					System.out.println("+------------------------+\n");
 					UbahBarang();
 			}
 
@@ -259,18 +282,22 @@ public class Barang extends koneksi implements KelolaBarang {
     	Class.forName("com.mysql.cj.jdbc.Driver");
 		conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
 		
-		System.out.println("\n ---------------------------------------------- ");
-		System.out.println("|                  HAPUS DATA                    |");
-		System.out.println(" ------------------------------------------------ ");
-		System.out.print("Masukkan SKU pada data yang akan di hapus : ");
+		System.out.print("\n\n");
+		System.out.println("+==========================================+");
+    		System.out.println("|               HAPUS DATA                 |");
+    		System.out.println("+==========================================+");
+		System.out.print("| Masukkan SKU pada data yang akan di hapus : ");
 		this.sku = input.nextLine();
+		System.out.println("+==========================================+");
 		
 		try {
 			stmt = conn.createStatement();
 			String sql = "DELETE FROM data_master WHERE sku='"+sku+"'";
 		
 			stmt.execute(sql);
-			System.out.println("\nData Berhasil Terhapus");
+			System.out.println("+------------------------+");
+			System.out.println("| Data Berhasil Terhapus |");
+			System.out.println("+------------------------+\n");
 
 		}
 		
@@ -295,9 +322,10 @@ public class Barang extends koneksi implements KelolaBarang {
         rs = stmt.executeQuery(sql);
 
         try {
-            System.out.println("\n ------------------------------------------------------------------------------------ ");
-            System.out.println("|                            DATA BARANG TOKO KELOMPOK 6                             |");
-            System.out.println(" ------------------------------------------------------------------------------------ ");
+            System.out.print("\n\n");
+    		System.out.println("+=====================================================+");
+        	System.out.println("|               DATA BARANG KELOMPOK 6                |");
+        	System.out.println("+=====================================================+");
             
          		System.out.print("  SKU");
          		System.out.print("\t\t");
@@ -358,9 +386,13 @@ public class Barang extends koneksi implements KelolaBarang {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
 		
-        System.out.println("\n ------------------------------------------------------------------------------------ ");
-        System.out.println("|                                 RESTOK BARANG                                     |");
-        System.out.println(" ------------------------------------------------------------------------------------ ");
+        	System.out.print("\n\n");
+		System.out.println("+=====================================================+");
+    		System.out.println("|               RESTOK BARANG KELOMPOK 6              |");
+    		System.out.println("+=====================================================+");
+		System.out.print("| Masukkan nama barang : ");
+		String cek = input.nextLine();
+		System.out.println("+=====================================================+");
         
 		System.out.print("Masukkan nama barang : ");
 		String cek = input.nextLine();
