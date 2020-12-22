f765fcbe73a03f182f2945a8ea894e503d8587bc
donihamdani <72194437+donihamdani@users.noreply.github.com>
2020-12-22 10:08:20 +0700

    Update Program.java

--- src/kasir/Program.java
+++ src/kasir/Program.java
@@ -6,29 +6,11 @@ import java.util.InputMismatchException;
 
 public class Program {
 
-	static Scanner scn = new Scanner(System.in);
-	static User user = new User();
-	static Date date = new Date();
+	static LogIn login = new LogIn();
+	
 	public static void main(String[] args) throws Exception {
 		
-		int pilihan; 
-		
-		switch (LogIn.landingPage()) {
-		case 1:
-			user.login();
-			break;
-
-		case 2:
-			user.TambahAkun();
-			break;
-		
-		default:
-			System.out.println("+------------------------+");
-			System.out.println("| Pilihan Tidak Tersedia |");
-			System.out.println("+------------------------+");
-			LogIn.landingPage();
-			break;
-		}
+		login.landingPage();
 	}
 
 }
