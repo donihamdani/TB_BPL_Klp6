a1585245c8d05bccb3a8a6bd2ca02223d6f17b5a
donihamdani <72194437+donihamdani@users.noreply.github.com>
2020-12-22 10:11:32 +0700

    Update LogIn.java
    .

--- src/kasir/LogIn.java
+++ src/kasir/LogIn.java
@@ -9,30 +9,42 @@ public class LogIn {
 	static Date date = new Date();
 	static Scanner scn = new Scanner(System.in);
 	
-	public static int landingPage() {
+	public static int landingPage() throws Exception {
 		
 		Integer pilihan = 0;
+		System.out.println("+============================================+");
+  		System.out.println("|             SELAMAT DATANG DI              |");
+  		System.out.println("|              TOKO KELOMPOK 6               |");
+  		System.out.println("+============================================+");
+		System.out.println("|        "+date+"        |");
+		System.out.println("+============================================+\n");
 		
-		System.out.println("       Toko Kelompok 6      ");
-		System.out.println("============================");
-		System.out.println(date+"\n");
-		System.out.println("1. Login");
-		System.out.println("2. Register");
-		System.out.print("Tentukan pilihanmu : ");
-		
-		try {
-			pilihan = scn.nextInt();
-		} catch (InputMismatchException e) {
-			System.out.println("Input yang anda masukkan salah");
+			System.out.println("+============================================+");
+			System.out.println("| [1] Login                                  |");
+			System.out.println("+============================================+");
+			System.out.println("| [2] Register                               |");
+			System.out.println("+============================================+");
+			System.out.print("| Pilih : ");
 			
-			System.out.println("       Toko Kelompok 6      ");
-			System.out.println("============================");
-			System.out.println(date+"\n");
-			System.out.println("1. Login");
-			System.out.println("2. Register");
-			System.out.print("Tentukan pilihanmu : ");
 			pilihan = scn.nextInt();
-		}
+			System.out.println("+============================================+");
+			
+			switch (pilihan) {
+			case 1:
+				user.login();
+				break;
+
+			case 2:
+				user.TambahAkun();
+				break;
+			
+			default:
+				System.out.println("+------------------------+");
+				System.out.println("| Pilihan Tidak Tersedia |");
+				System.out.println("+------------------------+\n");
+				landingPage();
+				break;
+			}
 		
 		return pilihan;
 	}
